ffc17baf8f83476737660af224dc28adde1adcbf
Mark Downey <mark@thebakery.io>
2018-10-25 23:42:45 +0100

    remove childkey prop filtering

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,7 +1,6 @@
 function getUnhandledProps(ComponentProps, props) {
   const handledProps = Object.keys(ComponentProps);
   return Object.keys(props).reduce((acc, prop) => {
-    if (prop === 'childkey') return acc;
     if (handledProps.indexOf(prop) === -1) acc[prop] = props[prop];
     return acc;
   }, {});
