0cc53184e36ba2ffff4f7c882883758f02e16ff6
Femi Oladeji <femidotexe@gmail.com>
2018-12-11 10:40:04 +0100

    fix bug with width being applied to all children divs

--- src/lib/components/slideshow/slide.js
+++ src/lib/components/slideshow/slide.js
@@ -20,7 +20,7 @@ class Slideshow extends Component {
   }
 
   componentDidMount() {
-    this.allImages = document.querySelectorAll(`.images-wrap div`);
+    this.allImages = document.querySelectorAll(`.images-wrap > div`);
     this.width = document.querySelector('.react-slideshow-wrapper').clientWidth;
     this.setWidth();
     window.addEventListener('resize', this.resizeListener);
