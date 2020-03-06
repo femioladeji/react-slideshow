ce8d203ee6521b02cc118576e9c60746c0a26cdb
Carlos Alonso <40870617+calorenzo@users.noreply.github.com>
2020-03-06 11:15:41 +0100

    Fixed extra spaces in App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       duration: 5000,
       transitionDuration: 500,
       infinite: true,
-      indicators: true,	  
+      indicators: true,
 	  pauseOnHover: true,
       onChange: (oldIndex, newIndex) => {
         console.log(
