1a1f54eb68a9b1c3ad747acb3130cd911c9034ea
martins obayomi <obayomimartins96@gmail.com>
2020-07-01 10:15:31 +0100

    Isolate examples

--- src/views/components/SideBar.js
+++ src/views/components/SideBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles.css';
 
@@ -14,27 +14,24 @@ const Sidebar = () => {
         <Link to="/api">
           <div className="menu-item">Api</div>
         </Link>
-        <div 
-          className="menu-item" 
-          onClick={() => setDropDowns(!dropDowns)}>
+        <div className="menu-item" onClick={() => setDropDowns(!dropDowns)}>
           Examples
         </div>
-        {
-          dropDowns === true ?
-          (
-            <div className="menu-item dropdown-container">
-              <Link to="/slide-effect">
-                <div className="d-item menu-item">Slide Effect</div>
-              </Link>
-              <Link to="/zoom-effect">
-                <div className="d-item menu-item">Zoom Effect</div>
-              </Link>
-              <Link to="/fade-effect">
-                <div className="d-item menu-item">Fade Effect</div>
-              </Link>
-            </div>
-          ) : ''
-        }
+        {dropDowns ? (
+          <div className="menu-item dropdown-container">
+            <Link to="/slide-effect">
+              <div className="d-item menu-item">Slide Effect</div>
+            </Link>
+            <Link to="/zoom-effect">
+              <div className="d-item menu-item">Zoom Effect</div>
+            </Link>
+            <Link to="/fade-effect">
+              <div className="d-item menu-item">Fade Effect</div>
+            </Link>
+          </div>
+        ) : (
+          ''
+        )}
         <Link to="/typescript">
           <div className="menu-item">For Typescript</div>
         </Link>
