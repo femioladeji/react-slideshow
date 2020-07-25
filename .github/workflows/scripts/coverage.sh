40346d180c48259cb2e04f0793830960fc3479a6
Femi Oladeji <f.oladeji@temper.works>
2020-07-25 13:08:59 +0200

    fix github test coverage

--- .github/workflows/scripts/coverage.sh
+++ .github/workflows/scripts/coverage.sh
@@ -3,7 +3,8 @@
 
 SCRIPT_RUN_COVERAGE=".github/workflows/scripts/run-coverage.js"
 
-npm i -g @actions/core @actions/github jest faker axios aws-sdk qs dotenv
+npm i -g @actions/core @actions/github
+npm i --only=dev
 npmGlobalModules=$( npm root -g | head -n 1 )
 export NODE_PATH=$npmGlobalModules
 
