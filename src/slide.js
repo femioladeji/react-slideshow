8e7f1d5567736a1d0fdaddfd542614dba9952564
Femi Oladeji <femidotexe@gmail.com>
2022-01-09 19:44:34 +0100

    don't render extra slides if not infinite

--- src/slide.js
+++ src/slide.js
@@ -102,7 +102,9 @@ class Slideshow extends Component {
     if (canSwipe) {
       const clientX = e.touches ? e.touches[0].pageX : e.clientX;
       if (this.dragging) {
-        let translateValue = this.width * (this.state.index + slidesToShow);
+        let translateValue =
+          this.width *
+          (this.state.index + this.getOffset(infinite, slidesToShow));
         const distance = clientX - this.startingClientX;
         if (
           !infinite &&
@@ -144,7 +146,7 @@ class Slideshow extends Component {
           0
         )) ||
       [];
-    const { slidesToShow } = getProps(this.props);
+    const { slidesToShow, infinite } = getProps(this.props);
     this.width =
       ((this.wrapper && this.wrapper.clientWidth) || 0) / slidesToShow;
     const numberOfSlides = React.Children.count(this.props.children);
@@ -152,7 +154,7 @@ class Slideshow extends Component {
     if (this.imageContainer) {
       this.imageContainer.style.width = `${fullwidth}px`;
       this.imageContainer.style.transform = `translate(-${this.width *
-        (this.state.index + slidesToShow)}px)`;
+        (this.state.index + this.getOffset(infinite, slidesToShow))}px)`;
     }
     this.applySlideStyle();
   }
@@ -287,14 +289,28 @@ class Slideshow extends Component {
     ));
   }
 
+  getOffset(infinite, slidesToShow) {
+    if (!infinite) {
+      return 0;
+    }
+    return slidesToShow;
+  }
+
   render() {
-    const { children, indicators, arrows, cssClass, slidesToShow } = getProps(
-      this.props
-    );
+    const {
+      children,
+      indicators,
+      arrows,
+      cssClass,
+      slidesToShow,
+      infinite
+    } = getProps(this.props);
     const unhandledProps = getUnhandledProps(propTypes, this.props);
     const { index } = this.state;
     const style = {
-      transform: `translate(-${(index + slidesToShow) * this.width}px)`
+      transform: `translate(-${(index +
+        this.getOffset(infinite, slidesToShow)) *
+        this.width}px)`
     };
 
     return (
@@ -328,7 +344,9 @@ class Slideshow extends Component {
               style={style}
               ref={ref => (this.imageContainer = ref)}
             >
-              {this.renderPreceedingSlides(children, slidesToShow)}
+              {infinite
+                ? this.renderPreceedingSlides(children, slidesToShow)
+                : ''}
               {children.map((each, key) => {
                 const isSlideActive = this.isSlideActive(key);
                 return (
@@ -343,7 +361,9 @@ class Slideshow extends Component {
                   </div>
                 );
               })}
-              {this.renderTrailingSlides(children, slidesToShow)}
+              {infinite
+                ? this.renderTrailingSlides(children, slidesToShow)
+                : ''}
             </div>
           </div>
           {arrows &&
@@ -381,11 +401,16 @@ class Slideshow extends Component {
       clearTimeout(this.timeout);
       const value = {
         margin:
-          -this.width * (this.state.index + slidesToShow) + this.distanceSwiped
+          -this.width *
+            (this.state.index + this.getOffset(infinite, slidesToShow)) +
+          this.distanceSwiped
       };
       const tween = new TWEEN.Tween(value, this.tweenGroup)
         .to(
-          { margin: -this.width * (index + slidesToShow) },
+          {
+            margin:
+              -this.width * (index + this.getOffset(infinite, slidesToShow))
+          },
           transitionDuration
         )
         .onUpdate(value => {
