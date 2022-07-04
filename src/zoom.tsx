e6fed6d38b769148a1dd40caaa7e176eb66ba4d7
Femi Oladeji <femidotexe@gmail.com>
2022-07-04 19:39:46 +0200

    style(lint): fix lint issues

--- src/zoom.tsx
+++ src/zoom.tsx
@@ -3,7 +3,7 @@ import { FadeZoom } from './fadezoom';
 import { defaultProps } from './props';
 import { SlideshowRef, ZoomProps } from './types';
 
-export const Zoom =  React.forwardRef<SlideshowRef, ZoomProps>((props, ref) => {
+export const Zoom = React.forwardRef<SlideshowRef, ZoomProps>((props, ref) => {
     return <FadeZoom {...props} ref={ref} />;
 });
 
